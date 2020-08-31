91866941900c47684c066eeac597be8ed1511cbd
TiffanyJan <tiffany_low@hotmail.com>
2020-08-31 21:50:03 +0100

    set layout for weather icons

--- src/App.js
+++ src/App.js
@@ -1,84 +1,82 @@
-import React,{useState} from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import sunImg from "./sun.png";
 import sunAndCloud from "./sunAndCloud.png";
 import sunAndRain from "./sunAndRain.jp.jpg";
 
 function App() {
-
-  const [dailyWeather,setDailyWeather]=useState([
-   {
-     day:"Monday",
-     forecast:"Sunny",
-     img:sunImg,
-   },
-   {
-     day:"Tuesday",
-     forecast:"Sunny",
-     img:sunImg,
-   },
-   {
-     day:"Wednesday",
-     forecast:"Cloudy with Sun",
-     img:sunAndCloud,
-   },
-   {
-     day:"Thursday",
-     forecast:"Sunny",
-     img:sunImg,
-   },
-   {
-    day:"Wednesday",
-    forecast:"Cloudy with Sun",
-    img:sunAndCloud,
-  },   {
-    day:"Thursday",
-    forecast:"Cloudy with Sun",
-    img:sunAndCloud,
-  },
-  {
-    day:"Friday",
-    forecast:"Cloudy with Sun",
-    img:sunAndCloud,
-  },
-  {
-    day:"Saturday",
-    forecast:"Rain",
-    img:sunAndRain,
-  },
-  {
-    day:"Sunday",
-    forecast:"Sunny",
-    img:sunImg,
-  }
+  const [dailyWeather, setDailyWeather] = useState([
+    {
+      day: "Monday",
+      forecast: "Sunny",
+      img: sunImg,
+    },
+    {
+      day: "Tuesday",
+      forecast: "Sunny",
+      img: sunImg,
+    },
+    {
+      day: "Wednesday",
+      forecast: "Cloudy with Sun",
+      img: sunAndCloud,
+    },
+    {
+      day: "Thursday",
+      forecast: "Sunny",
+      img: sunImg,
+    },
+    {
+      day: "Wednesday",
+      forecast: "Cloudy with Sun",
+      img: sunAndCloud,
+    },
+    {
+      day: "Thursday",
+      forecast: "Cloudy with Sun",
+      img: sunAndCloud,
+    },
+    {
+      day: "Friday",
+      forecast: "Cloudy with Sun",
+      img: sunAndCloud,
+    },
+    {
+      day: "Saturday",
+      forecast: "Rain",
+      img: sunAndRain,
+    },
+    {
+      day: "Sunday",
+      forecast: "Sunny",
+      img: sunImg,
+    },
   ]);
 
   return (
     <>
-    <div className="heading">
-    <h1>Weather</h1>
-    </div>
-    <div className="container">
-    <div className="row mt-2">
+      <div className="heading">
+        <h1>Weather</h1>
+      </div>
+      <div className="container">
         {dailyWeather.map((weather) => (
           <div key={weather.day} style={{ margin: "0 auto" }}>
-            
-            <div className="row mt-2">{weather.day}</div>
+            <div className="col-sm-1">
+              <div className="col-sm-1">{weather.day}</div>
             </div>
-            <div className="row mt-2">
-                <img
-                  className="card-img-top"
-                  src={weather.img}
-                  alt="Image"
-                  style={{ width: "8rem", margin: "0" }}
-                ></img>
+            <div className="col-sm-1">
+              <img
+                className="card-img-top"
+                src={weather.img}
+                alt="Image"
+                style={{ width: "8rem", margin: "0" }}
+              ></img>
             </div>
           </div>
         ))}
       </div>
-      </div>
-     </>
+    </>
   );
 }
 
