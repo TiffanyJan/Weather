dd8298a6a9cf0b0548afe6ae315f9e6494022e55
TiffanyJan <tiffany_low@hotmail.com>
2020-08-31 12:42:14 +0100

    create object for daily weather

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import React from 'react';
-
+import {React,useState} from 'react';
 import './App.css';
+import sunImg from "./sun.png";
+import sunAndCloud from "./sunAndCloud.png";
+import sunAndRain from "./sunAndRain.jp.jpg";
 
 function App() {
+
+  const [dailyWeather,setDailyWeather]=useState([
+   {
+     day:"Monday",
+     forecast:"Sunny",
+     img:sunImg,
+   },
+   {
+     day:"Tuesday",
+     forecast:"Sunny",
+     img:sunImg,
+   },
+   {
+     day:"Wednesday",
+     forecast:"Cloudy with Sun",
+     img:sunAndCloud,
+   },
+   {
+     day:"Thursday",
+     forecast:"Sunny",
+     img:sunImg,
+   },
+   {
+    day:"Wednesday",
+    forecast:"Cloudy with Sun",
+    img:sunAndCloud,
+  },   {
+    day:"Thursday",
+    forecast:"Cloudy with Sun",
+    img:sunAndCloud,
+  },
+  {
+    day:"Friday",
+    forecast:"Cloudy with Sun",
+    img:sunAndCloud,
+  },
+  {
+    day:"Saturday",
+    forecast:"Rain",
+    img:sunAndRain,
+  },
+  {
+    day:"Sunday",
+    forecast:"Sunny",
+    img:sunImg,
+  }
+  ]);
+
+
   return (
-<div>Hello World</div>
+    <>
+    <div className="heading">
+    <h1>Weather</h1>
+    </div>
+     <div className="row mt-2">
+
+     </div>
+     </>
   );
 }
 
